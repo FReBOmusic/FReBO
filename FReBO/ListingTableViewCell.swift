e59045c9c2e42a0f7c6541def40c06facba5ce62
FreBOMusic <frebomusic@divines-iMac.local>
2017-08-15 23:00:59 -0500

    Created `ListingTableViewCell`.

---
+++ FReBO/ListingTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ListingTableViewCell.swift
+//  FReBO
+//
+//  Created by FreBOMusic on 8/15/17.
+//  Copyright © 2017 FReBOMusic. All rights reserved.
+//
+
+import UIKit
+
+class ListingTableViewCell: UITableViewCell {
+    
+}
