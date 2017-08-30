90c72dba68e39237573cb9a40097a3690891ad9a
FreBOMusic <frebomusic@divines-iMac.local>
2017-08-30 11:19:34 -0500

    Added `FBRequest` and `FBListing` classes. Introduced error with `_OptionalNilComparisonType`. See: https://bugs.swift.org/browse/SR-4795

---
+++ FReBO/FBRequest.swift
@@ -0,0 +1,20 @@
+//
+//  FBRequest.swift
+//  FReBO
+//
+//  Created by FreBOMusic on 8/30/17.
+//  Copyright © 2017 FReBOMusic. All rights reserved.
+//
+
+import Foundation
+
+class FBRequest {
+    
+    init(search: String, count: Int=0) {
+        
+    }
+    
+    func fetchListings(handler: @escaping (([FBListing]) -> Void)) {
+        
+    }
+}
