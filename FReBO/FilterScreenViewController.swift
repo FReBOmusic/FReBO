5335d44b909ba9ac5bab04ed1195e40668fffd53
FreBOMusic <frebomusic@divines-iMac.local>
2017-09-27 13:54:10 -0500

    Added `FilterScreenViewController` & `FilterScreenTableViewController`

---
+++ FReBO/FilterScreenViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FilterScreenViewController.swift
+//  FReBO
+//
+//  Created by FreBOMusic on 9/27/17.
+//  Copyright © 2017 FReBOMusic. All rights reserved.
+//
+
+import UIKit
+
+class FilterScreenViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let filterTableViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Filter Table")
+        addChildViewController(filterTableViewController)
+        tableView = filterTableViewController.view.subviews.first as! UITableView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
